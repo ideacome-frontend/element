691eb93ca5ced34169072d7811114a2b5b6e89c6
wf <feng.wang@ideacome.com>
2020-09-23 18:48:41 +0800

    [feature]tree判断checkbox是否可见

--- packages/tree/src/tree-node.vue
+++ packages/tree/src/tree-node.vue
@@ -31,7 +31,7 @@
         :class="{ 'is-leaf': node.isLeaf, expanded: !node.isLeaf && expanded }">
       </span>
       <el-checkbox
-        v-if="showCheckbox && node.data.showNodeCheckbox"
+        v-if="showCheckbox && node.showNodeCheckbox"
         v-model="node.checked"
         :indeterminate="node.indeterminate"
         :disabled="!!node.disabled"
